849c173e8269708074df3a2b962fbd1a6163d0f1
Moshood1426 <moshood.yemi10@gmail.com>
2023-08-01 14:53:48 -0400

    Vandor model initiated

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { AdminRoute, VandorRoute } from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/api/v1/admin", AdminRoute);
 app.use("/api/v1/vandor", VandorRoute);
-
-
 
 app.listen(5000, () => {
   console.log("app is running on port 5000");
