b5c301a152972bdda32da3123c4b745cf4f0f21e
Moshood1426 <moshood.yemi10@gmail.com>
2023-08-01 14:39:12 -0400

    functions added in admin controller and attributed to respective routes in admin route

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -1,9 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 
-export const CreateVandor = async (
+export const createVandor = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-    
-};
+) => {};
+
+export const getAllVandor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const getVandorById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,5 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
+import {
+  createVandor,
+  getAllVandor,
+  getVandorById,
+} from "../controllers/adminController";
 
 const router = express.Router();
+
+router.get("/vandor", getAllVandor);
+router.post("/vandor", createVandor);
+router.get("/vandor/:id", getVandorById);
 
 export { router as AdminRoute };
